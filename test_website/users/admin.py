7ef443d0bbae7fddb70edf8297f85679f8571495
frank <frank.danil704@gmail.com>
2024-04-21 17:14:18 +0300

    update user auth_model

--- test_website/users/admin.py
+++ test_website/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from users.models import User
+from .choices import EXPERIENCE_TYPES
+
+User = get_user_model()
 
 
 class QualifyExecutorsFilter(admin.SimpleListFilter):
@@ -8,13 +11,7 @@ class QualifyExecutorsFilter(admin.SimpleListFilter):
     parameter_name = 'qualify'
 
     def lookups(self, request, model_admin):
-        return [
-            ('Нет опыта', 'Нет опыта'),
-            ('Несколько месяцев', 'Несколько месяцев'),
-            ('От 1 до 3 лет', 'От 1 до 3 лет'),
-            ('От 3 до 5 лет', 'От 3 до 5 лет'),
-            ('Более 5 лет', 'Более 5 лет')
-        ]
+        return EXPERIENCE_TYPES
 
     def queryset(self, request, queryset):
         if self.value():
